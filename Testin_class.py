1b4ede7197fa3525364e15f8e5e7bb8643847c87
obadaq <obadaq@ppu.edu>
2022-11-01 08:45:38 +0200

    Final Commit

--- Testin_class.py
+++ Testin_class.py
@@ -1,4 +1,4 @@
-from test import Date
+from Date import Date
 print(" This Code is a test code for newly defined CLASS Named 'Date' >>>")
 
 dd, mm, yyyy = input('Enter a date in this form - dd,mm,yyyy   >>:  ').split(',')
@@ -14,13 +14,11 @@ if d1.valid():
 
     add = int(input('Enter a number to add to the Date you Entered  >>:  '))
     d2 = d1 + add
-    print(d2)
-    print(d1)
     print ('This Date >>:  ', d2, '  >>:is after the date you Entered by',add)
     dd, mm, yyyy = input('Enter First Date in this form - dd,mm,yyyy   >>:  ').split(',')
     d3 = Date(int(dd), int(mm), int(yyyy))
     dd, mm, yyyy = input('Enter Second Date in this form - dd,mm,yyyy   >>:  ').split(',')
     d4 = Date(int(dd), int(mm), int(yyyy))
     d5 = d3 - d4
-    print('The absolute difference between>>:  ',d3,'and >>:   ',d4, '===')
+    print('The absolute difference between>>:  ',d3,'and >>:   ',d4, '===',d5)
     print('you Can use the class to compare any date with a number representing order')
